6469c9b90a9242e939101192932fa1e56ee225a3
Krishna Karthik Gadiraju <kgadira@stac-lab1.csc.ncsu.edu>
2017-01-10 14:46:06 -0500

    update mlc code

--- classification/mlc.R
+++ classification/mlc.R
@@ -11,6 +11,10 @@ rm(list=ls(all=T))
 library(rgdal)
 library(rgeos)
 
+
+
+covDets <- rep(0,4)
+covInv <- list()
 calculateMeans <- function(data){
   means <- apply(data[,-1],2, mean)
   means  
@@ -22,11 +26,24 @@ calculateCov <- function(data){
 }
 
 disc <- matrix(0,3504900,4)
-calculateDiscriminant <- function(j,d,means,covs,apriori){
+calculateDiscriminant <- function(j,d,means,apriori){
+  current <- d[j,-1]
+  sum = 0;
   for(i in 1:4){
-  disc[j,i] <<- -0.5*(as.matrix(d[j,-1]-means[[i]])%*%(solve(covs[[i]]))%*%(t(as.matrix(d[j,-1]-means[[i]]))))+log(apriori[i])-(5/2)*log(2*pi)- (1/2)*det(covs[[i]])
-  print(paste(j,i, disc[j,i]))
+  
+  currentMean <- means[[i]]
+  currentMat <- as.matrix(current - currentMean)
+  disc[j,i] <<- -0.5*(currentMat)%*%(solve(covInv[[i]]))%*%(t(currentMat))+log(apriori[i])-(5/2)*log(2*pi)- (1/2)*covDets[i]
+  sum = sum + disc[j,i];
 	}
+  for(k in 1:4){
+      disc[j,k] <<- disc[j,k]/sum;
+      	 
+  if(j%%100000==0){
+  print(paste(j,k, disc[j,k]))
+  		}
+ 
+   }
 }
 
 
@@ -45,9 +62,14 @@ MLE <- function(training){
 
   covs <- lapply(classSplit,calculateCov) 
   
+  for(i in 1:4){
+    covDets[i] <<- log(det(covs[[i]]))
+    covInv[[i]] <<- solve(covs[[i]])
+  }
   print('covs calculated')
   
-  print(covs)
+  print(covDets)
+  print(covInv)
   apriori <- rep(0,4)
   for(i in 1:4){
     apriori[i] <- nrow(classSplit[[i]])/(nrow(classSplit[[1]])+nrow(classSplit[[2]])+nrow(classSplit[[3]])+nrow(classSplit[[4]]))
@@ -55,7 +77,7 @@ MLE <- function(training){
 
   data.1 <- read.csv('../training/2015-04-19-AllBands.csv')
   
-  sapply(1:nrow(data.1), calculateDiscriminant, data.1,means, covs, apriori)
+  sapply(1:nrow(data.1), calculateDiscriminant, data.1,means, apriori)
   
  write.csv(file='../training/mlc-results.csv',x = disc, row.names=F)  
   
